3a00f2e27d4d2a6c34b552f0b998d09df2b6e3f5
Anikait Prasar <anikait1892@gmail.com>
2024-06-29 21:34:36 +0530

    detecting wall collisions and changing direction of ball

--- script.js
+++ script.js
@@ -9,17 +9,46 @@ let stickHeight = 60;
 
 let y_coordinate1 = Math.random() * (window.innerHeight-stickWidth);
 let y_coordinate2 = Math.random() * (window.innerHeight-stickWidth);
-let x_coordinate1 = Math.random() * (window.innerWidth-stickHeight)
-let x_coordinate2 = Math.random() * (window.innerWidth-stickHeight)
+
+let ball_x = 100
+let ball_y = 100
+let ball_radius = 15
+
+let ball_dx = (Math.random()*1)+2;
+let ball_dy = (Math.random()*1)+3;
 
 
 ctx.fillStyle = '#202022';
 
 ctx.fillRect(3, y_coordinate1, stickWidth, stickHeight);
 ctx.fillRect(window.innerWidth-stickWidth-3, y_coordinate2, stickWidth, stickHeight);
-ctx.fillRect(x_coordinate1, 3, stickHeight, stickWidth);
-ctx.fillRect(x_coordinate2, window.innerHeight-stickWidth-3, stickHeight, stickWidth);
 
 ctx.fillStyle = '#878787';
-ctx.arc(100, 100, 15, 0, 2*Math.PI);
+ctx.arc(ball_x, ball_y, ball_radius, 0, 2*Math.PI);
 ctx.fill();
+
+window.addEventListener("click", setGameInterval);
+
+function setGameInterval(){
+    setInterval(gameLoop, 10)
+}
+
+function clearCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
+
+function update(){
+    if (ball_y > canvas.height || ball_y < 0) {
+        ball_dy = -ball_dy;
+    }
+    ball_x+=ball_dx;
+    ball_y+=ball_dy;
+    ctx.beginPath();
+    ctx.arc(ball_x, ball_y, ball_radius, 0, 2*Math.PI);
+    ctx.fill();
+}
+
+function gameLoop(){
+    clearCanvas();
+    update();
+}
